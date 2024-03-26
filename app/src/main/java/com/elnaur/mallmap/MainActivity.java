fe83357528ef067a1e26563d1c9e22c66d35306d
Elnaur <nicole.c.loubser@gmail.com>
2024-03-26 18:38:49 +0200

    Initial commit

---
+++ app/src/main/java/com/elnaur/mallmap/MainActivity.java
@@ -0,0 +1,18 @@
+package com.elnaur.mallmap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * References:
+ * <a href="https://github.com/Baseflow/PhotoView">...</a>
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
